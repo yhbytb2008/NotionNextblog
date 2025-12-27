ac348c9f7f978cb67e276178cf5372d05020bd69
yang <55042474+yhbytb2008@users.noreply.github.com>
2025-12-27 11:16:41 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -11,7 +11,7 @@ export default function PoweredBy(props) {
       <a
         href='https://bk.qqsea.top/'
         className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
+        我的天空 {siteConfig('VERSION')}
       </a>
       .
     </div>
